be17b3ba60b72e67514d34134e6d43d1dc420417
Levy <golvwei@gmail.com>
2023-12-28 23:39:04 +0800

    Tool-001: shadcn/ui and lucide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
 import MaxWidthWrapper from "~/components/MaxWidthWrapper";
+import { buttonVariants } from "~/components/ui/button";
+import { ArrowRight } from "lucide-react";
 import { cn } from "~/lib/utils";
 
 export default function Home() {
@@ -12,13 +15,27 @@ export default function Home() {
         )}
       >
         <p className="text-sm font-semibold text-gray-700">
-          Levy is now public!
+          Molly is now public!
         </p>
       </div>
       <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
         Chat with your <span className="text-blue-600">documents</span> in
         seconds.
       </h1>
+      <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+        Molly allows you to have conversations with any PDF document. Simply
+        upload your file and start asking questions right away
+      </p>
+      <Link
+        className={buttonVariants({
+          size: "lg",
+          className: "mt-5",
+        })}
+        href="/dashboard"
+        target="_blank"
+      >
+        Get started <ArrowRight className="ml-2 h-5 w-5" />
+      </Link>
     </MaxWidthWrapper>
   );
 }
