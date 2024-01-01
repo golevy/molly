714dbe698ddbd9ddec85efa8f631c5f4105565c3
Levy <golvwei@gmail.com>
2024-01-01 21:11:34 +0800

    Feature-002: Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -153,7 +153,7 @@ export default function Home() {
                 Start asking questions
               </span>
               <span className="mt-2 text-zinc-700">
-                It&apos;s that simple. Try out Quill today - it really takes
+                It&apos;s that simple. Try out Molly today - it really takes
                 less than a minute.
               </span>
             </div>
