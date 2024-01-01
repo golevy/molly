8395f58935ccb2cf4184e28b89e9aa80111607f1
Levy <golvwei@gmail.com>
2024-01-01 23:52:49 +0800

    Feature-003: Auth Dialog (#12)

---
+++ src/components/auth/AuthDialog.tsx
@@ -0,0 +1,15 @@
+import { Dialog, DialogContent } from "~/components/ui/dialog";
+import { DialogProps } from "@radix-ui/react-dialog";
+import AuthForm from "~/components/auth/AuthForm";
+
+const AuthDialog = ({ open, onOpenChange }: DialogProps) => {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="flex flex-col sm:max-w-96">
+        <AuthForm />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AuthDialog;
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,6 +5,7 @@ import {
   type NextAuthOptions,
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -47,15 +48,10 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
   ],
 };
 
